ae28106304b55f61b76e48d18425835ce7c42b42
Felicia Weathers <Fel.Weathers@gmail.com>
2016-08-29 12:33:25 -0400

    Add SecondViewController and buttons to segue back and forth

---
+++ SecondViewController.swift
@@ -0,0 +1,35 @@
+//
+//  SecondViewController.swift
+//  AdvancedSegues
+//
+//  Created by Felicia Weathers on 8/29/16.
+//  Copyright © 2016 Felicia Weathers. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
